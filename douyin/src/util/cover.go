43adcec20f01066910d0b317245b40a4f5cb1749
wuyou <2213093478@qq.com>
2022-06-05 15:50:26 +0800

    add 新增截取视频封面工具类

---
+++ douyin/src/util/cover.go
@@ -0,0 +1,47 @@
+package util
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/disintegration/imaging"
+	ffmpeg "github.com/u2takey/ffmpeg-go"
+	"log"
+	"os"
+)
+
+// GetSnapshot 根据视频生成封面cover存储到本地
+// frameNum 第几帧
+func GetSnapshot(videoPath, snapshotPath string, frameNum int) (err error) {
+	// 读取本地文件流
+	buf := bytes.NewBuffer(nil)
+	err = ffmpeg.Input(videoPath).
+		Filter(
+			"select",
+			ffmpeg.Args{fmt.Sprintf("gte(n,%d)", frameNum)}).
+		Output(
+			"pipe:",
+			ffmpeg.KwArgs{"vframes": 1, "format": "image2", "vcodec": "mjpeg"}).
+		WithOutput(
+			buf,
+			os.Stdout).
+		Run()
+	if err != nil {
+		log.Fatal("read local file err {}", err)
+		return err
+	}
+
+	// 将视频帧转换为cover
+	img, err := imaging.Decode(buf)
+	if err != nil {
+		log.Fatal("decode buffer as cover err {}", err)
+		return err
+	}
+
+	// 将cover存储到本地
+	err = imaging.Save(img, snapshotPath)
+	if err != nil {
+		log.Fatal("cover store local err {}", err)
+		return err
+	}
+	return nil
+}
