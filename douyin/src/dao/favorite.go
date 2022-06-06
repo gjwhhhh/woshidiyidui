c779ee3b112ad07b80d87e72db8b9c3e48f006f5
weixizi <weixizi9@gmail.com>
2022-06-06 22:28:14 +0800

    增加对脏数据的处理

--- douyin/src/dao/favorite.go
+++ douyin/src/dao/favorite.go
@@ -1,6 +1,7 @@
 package dao
 
 import (
+	"database/sql"
 	"douyin/src/global"
 	"douyin/src/pkg/errcode"
 	"douyin/src/pojo/vo"
@@ -27,13 +28,59 @@ WHERE
     dy_user.isdelete = 0
 	`
 
-func FindFavoriteVideoListByUId(uid int64) ([]vo.Video, error) {
+type Video struct {
+	Id            sql.NullInt64  `json:"id,omitempty"`
+	Author        User           `json:"author"`
+	PlayUrl       sql.NullString `json:"play_url" json:"play_url,omitempty"`
+	CoverUrl      sql.NullString `json:"cover_url,omitempty"`
+	FavoriteCount sql.NullInt64  `json:"favorite_count,omitempty"`
+	CommentCount  sql.NullInt64  `json:"comment_count,omitempty"`
+}
 
+type User struct {
+	Id            sql.NullInt64  `json:"id,omitempty"`
+	Name          sql.NullString `json:"name,omitempty"`
+	FollowCount   sql.NullInt64  `json:"follow_count,omitempty"`
+	FollowerCount sql.NullInt64  `json:"follower_count,omitempty"`
+}
+
+// NewVoVideo 根据video获取vo.Video
+func (v Video) NewVoVideo() *vo.Video {
+	if !v.Id.Valid && !v.PlayUrl.Valid && !v.CoverUrl.Valid && !v.FavoriteCount.Valid && !v.CommentCount.Valid {
+		return nil
+	}
+	return &vo.Video{
+		Id:            v.Id.Int64,
+		PlayUrl:       v.PlayUrl.String,
+		CoverUrl:      v.CoverUrl.String,
+		FavoriteCount: v.FavoriteCount.Int64,
+		CommentCount:  v.CommentCount.Int64,
+	}
+}
+
+// NewVoUser 根据User获取vo.User
+func (u User) NewVoUser() *vo.User {
+	if !u.Id.Valid && !u.Name.Valid && !u.FollowCount.Valid && !u.FollowerCount.Valid {
+		return nil
+	}
+	return &vo.User{
+		Id:            u.Id.Int64,
+		Name:          u.Name.String,
+		FollowCount:   u.FollowCount.Int64,
+		FollowerCount: u.FollowerCount.Int64,
+	}
+}
+
+// FindFavoriteVideoListByUId 根据用户id获取点赞的视频
+func FindFavoriteVideoListByUId(uid int64) ([]vo.Video, error) {
 	followerIdsChan := make(chan map[int64]struct{})
 	errorChan := make(chan error)
-	timer := time.NewTimer(time.Second)
-	go findFollowerIdsByFollowing(followerIdsChan, errorChan, uid)
 	var followerIdMap map[int64]struct{}
+	// 定义查询超时时间
+	timer := time.NewTimer(time.Second)
+	// 启用协程查询用户的关注列表
+	go findFollowerIdsByFollowing(followerIdsChan, errorChan, uid)
+	// 根据用户id获取点赞的视频
 	db := global.DBEngine
 	rows, err := db.DB().Query(FIND_FAVORITE_VIDEO_LIST_BY_UID_SQL, uid)
 	videos := make([]vo.Video, 0)
@@ -41,7 +88,7 @@ func FindFavoriteVideoListByUId(uid int64) ([]vo.Video, error) {
 		return videos, err
 	}
 	defer rows.Close()
-
+	// 等待协程结果，超时直接返回Timeout
 loop:
 	for {
 		select {
@@ -58,17 +105,25 @@ loop:
 			}
 		}
 	}
-
+	// 读取数据并判断是否关注
 	for rows.Next() {
-		var video vo.Video
-		var user vo.User
+		var video Video
+		var user User
 		if err = rows.Scan(&video.Id, &video.PlayUrl, &video.CoverUrl, &video.FavoriteCount, &video.CommentCount, &user.Id, &user.Name, &user.FollowCount, &user.FollowerCount); err != nil {
 			return videos, err
 		}
-		video.IsFavorite = true
-		_, user.IsFollow = followerIdMap[user.Id]
+		voUser := user.NewVoUser()
+		if voUser == nil {
+			continue
+		}
+		voVideo := video.NewVoVideo()
+		if voVideo == nil {
+			continue
+		}
+		voVideo.IsFavorite = true
+		_, voUser.IsFollow = followerIdMap[voUser.Id]
 		video.Author = user
-		videos = append(videos, video)
+		videos = append(videos, *voVideo)
 	}
 	return videos, nil
 }
--- douyin/src/dao/relation.go
+++ douyin/src/dao/relation.go
@@ -8,7 +8,7 @@ import (
 func FindFollowerIdsByFollowing(following int64) ([]int64, error) {
 	db := global.DBEngine
 	var followerIds []int64
-	if err := db.Table("dy_relation").Where("follower_id").Find(&followerIds).Error; err == nil || err == gorm.ErrRecordNotFound {
+	if err := db.Table("dy_relation").Where("follower_id = ?", following).Find(&followerIds).Error; err == nil || err == gorm.ErrRecordNotFound {
 		return followerIds, nil
 	} else {
 		return nil, err
