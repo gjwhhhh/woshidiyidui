7c1d551c1271f5d372231bad5df47880270e322a
gjw <gjw7a1712739@foxmail.com>
2022-06-06 15:08:34 +0800

    实现video DAO部分

--- douyin/src/dao/video.go
+++ douyin/src/dao/video.go
@@ -1,35 +1,206 @@
 package dao
 
-import "douyin/src/pojo/vo"
-
-// TODO 编写DAO
+import (
+	"douyin/src/global"
+	"douyin/src/pojo/entity"
+	"douyin/src/pojo/vo"
+	fmt "fmt"
+	"github.com/jinzhu/gorm"
+	"time"
+)
 
 // BatchVideoByTimeAndUId 通过时间和用户id获取视频流，需要指定用户是否点赞
 func BatchVideoByTimeAndUId(latestTime, userId int64, pageSize int) ([]vo.Video, error) {
-	return make([]vo.Video, 0), nil
+	var dyVideoList []entity.DyVideo
+	var db = global.DBEngine
+	sqlTimeFormat := timeunixTotimeString(latestTime)
+	//查询视频list
+	err := db.Where("create_date<?", sqlTimeFormat).Order("create_date DESC").Limit(pageSize).Find(&dyVideoList).Error
+	if err != nil {
+		return make([]vo.Video, 0), err
+	}
+
+	//转换voList
+	var videoVoList = make([]vo.Video, 0)
+	for i := 0; i < len(dyVideoList); i++ {
+		var userTmp entity.DyUser
+		db.Where("id=?", dyVideoList[i].UserId).Find(&userTmp)
+		var dyFavorite entity.DyFavorite
+		err := db.Where("user_id=? AND video_id=?", userId, dyVideoList[i].Id).Find(&dyFavorite).Error
+		var isFavorite = false
+		if err != gorm.ErrRecordNotFound {
+			isFavorite = true
+		}
+		videoTmp := vo.Video{
+			Id: dyVideoList[i].Id,
+			Author: vo.User{
+				Id:            userTmp.Id,
+				Name:          userTmp.Username,
+				FollowCount:   int64(userTmp.FollowerCount),
+				FollowerCount: int64(userTmp.FollowerCount),
+				IsFollow:      false,
+			},
+			PlayUrl:       dyVideoList[i].PlayUrl,
+			CoverUrl:      dyVideoList[i].CoverUrl,
+			FavoriteCount: int64(dyVideoList[i].FavoriteCount),
+			CommentCount:  int64(dyVideoList[i].CommentCount),
+			IsFavorite:    isFavorite,
+		}
+		videoVoList = append(videoVoList, videoTmp)
+	}
+	return videoVoList, nil
 }
 
 // BatchVideoByTime 通过时间获取视频流，不需要指定用户是否点赞
 func BatchVideoByTime(latestTime int64, pageSize int) ([]vo.Video, error) {
-	return make([]vo.Video, 0), nil
+	var dyVideoList []entity.DyVideo
+	var db = global.DBEngine
+	sqlTimeFormat := timeunixTotimeString(latestTime)
+	//查询视频list
+	err := db.Where("create_date<?", sqlTimeFormat).Order("create_date DESC").Limit(pageSize).Find(&dyVideoList).Error
+	if err != nil {
+		return make([]vo.Video, 0), err
+	}
+
+	//转换voList
+	var videoVoList = make([]vo.Video, 0)
+	for i := 0; i < len(dyVideoList); i++ {
+		var userTmp entity.DyUser
+		db.Where("id=?", dyVideoList[i].UserId).Find(&userTmp)
+		videoTmp := vo.Video{
+			Id: dyVideoList[i].Id,
+			Author: vo.User{
+				Id:            userTmp.Id,
+				Name:          userTmp.Username,
+				FollowCount:   int64(userTmp.FollowerCount),
+				FollowerCount: int64(userTmp.FollowerCount),
+				IsFollow:      false,
+			},
+			PlayUrl:       dyVideoList[i].PlayUrl,
+			CoverUrl:      dyVideoList[i].CoverUrl,
+			FavoriteCount: int64(dyVideoList[i].FavoriteCount),
+			CommentCount:  int64(dyVideoList[i].CommentCount),
+			IsFavorite:    false,
+		}
+		videoVoList = append(videoVoList, videoTmp)
+	}
+	return videoVoList, nil
+}
+
+//将时间戳转换为数据库时间格式
+func timeunixTotimeString(lastestTime int64) string {
+	timeObj := time.UnixMilli(lastestTime)
+
+	year := timeObj.Year()     //年
+	month := timeObj.Month()   //月
+	day := timeObj.Day()       //日
+	hour := timeObj.Hour()     //小时
+	minute := timeObj.Minute() //分钟
+	second := timeObj.Second() //秒
+	timeString := fmt.Sprintf("%d-%02d-%02d %02d:%02d:%02d\n", year, month, day, hour, minute, second)
+	return timeString
 }
 
 // GetVideoTimeById 通过视频id获取视频发布时间
 func GetVideoTimeById(userId int64) (int64, error) {
-	return 0, nil
+	var dyVideo entity.DyVideo
+	var db = global.DBEngine
+	err := db.Where("id=?", userId).Find(&dyVideo).Error
+	if err != nil {
+		return 0, err
+	}
+	return dyVideo.CreateDate.Unix(), nil
 }
 
 // AddVideo 用户新增视频
 func AddVideo(userId int64, videoUrl, coverUrl, title string) error {
+	dyVideo := entity.DyVideo{
+		UserId:   userId,
+		PlayUrl:  videoUrl,
+		CoverUrl: coverUrl,
+		Title:    title,
+	}
+	var db = global.DBEngine
+	err := db.Create(&dyVideo).Error
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 // BatchVideoByUId 批量查询视频信息，不需要指定是否点赞
 func BatchVideoByUId(userId int64) ([]vo.Video, error) {
-	return make([]vo.Video, 0), nil
+	var dyVideoList []entity.DyVideo
+	var db = global.DBEngine
+	err := db.Where("user_id=?", userId).Find(&dyVideoList).Error
+	if err != nil {
+		return make([]vo.Video, 0), err
+	}
+	var videoVoList []vo.Video
+	for _, video := range dyVideoList {
+		var userTmp entity.DyUser
+		db.Where("id=?", video.UserId).Find(&userTmp)
+
+		videoVoTmp := vo.Video{
+			Id: video.Id,
+			Author: vo.User{
+				Id:            userTmp.Id,
+				Name:          userTmp.Username,
+				FollowCount:   int64(userTmp.FollowCount),
+				FollowerCount: int64(userTmp.FollowerCount),
+				IsFollow:      false,
+			},
+			PlayUrl:       video.PlayUrl,
+			CoverUrl:      video.CoverUrl,
+			FavoriteCount: int64(video.FavoriteCount),
+			CommentCount:  int64(video.CommentCount),
+			IsFavorite:    false,
+		}
+		videoVoList = append(videoVoList, videoVoTmp)
+	}
+	return videoVoList, nil
 }
 
 // BatchVideoByUIdAndOtherUId 批量查询otherUId对应视频信息，需要通过curUserId指定是否点赞
 func BatchVideoByUIdAndOtherUId(curUserId, otherUId int64) ([]vo.Video, error) {
-	return make([]vo.Video, 0), nil
+	var dyVideoList []entity.DyVideo
+	var db = global.DBEngine
+	err := db.Where("user_id=?", curUserId).Find(&dyVideoList).Error
+	if err != nil {
+		return make([]vo.Video, 0), err
+	}
+	var videoVoList []vo.Video
+	for _, video := range dyVideoList {
+		var userTmp entity.DyUser
+		db.Where("id=?", video.UserId).Find(&userTmp)
+		var dyRelation entity.DyRelation
+		err := db.Where("follower_id=? AND following_id=?", curUserId, otherUId).Find(&dyRelation).Error
+		var isFollow = false
+		if err != gorm.ErrRecordNotFound {
+			isFollow = true
+		}
+		var dyFavorite entity.DyFavorite
+		err2 := db.Where("user_id=? AND video_id=?", curUserId, video.Id).Find(&dyFavorite).Error
+		var isFavorite = false
+		if err2 != gorm.ErrRecordNotFound {
+			isFavorite = true
+		}
+		videoVoTmp := vo.Video{
+			Id: video.Id,
+			Author: vo.User{
+				Id:            userTmp.Id,
+				Name:          userTmp.Username,
+				FollowCount:   int64(userTmp.FollowCount),
+				FollowerCount: int64(userTmp.FollowerCount),
+				IsFollow:      isFollow,
+			},
+			PlayUrl:       video.PlayUrl,
+			CoverUrl:      video.CoverUrl,
+			FavoriteCount: int64(video.FavoriteCount),
+			CommentCount:  int64(video.CommentCount),
+			IsFavorite:    isFavorite,
+		}
+		videoVoList = append(videoVoList, videoVoTmp)
+	}
+	return videoVoList, nil
 }
--- douyin/src/pojo/entity/dyVideo.go
+++ douyin/src/pojo/entity/dyVideo.go
@@ -1,21 +1,20 @@
 package entity
 
 import (
-	"database/sql"
 	"time"
 )
 
 type DyVideo struct {
-	Id            int64          `gorm:"column:id;primary_key;AUTO_INCREMENT" json:"id"`
-	UserId        sql.NullString `gorm:"column:user_id" json:"user_id"`
-	PlayUrl       sql.NullString `gorm:"column:play_url" json:"play_url"`
-	CoverUrl      sql.NullString `gorm:"column:cover_url" json:"cover_url"`
-	FavoriteCount sql.NullInt32  `gorm:"column:favorite_count;default:0" json:"favorite_count"`
-	CommentCount  sql.NullInt32  `gorm:"column:comment_count;default:0" json:"comment_count"`
-	Title         sql.NullString `gorm:"column:title" json:"title"`
-	CreateDate    time.Time      `gorm:"column:create_date;default:CURRENT_TIMESTAMP;NOT NULL" json:"create_date"` // 创建时间
-	UpdateDate    time.Time      `gorm:"column:update_date;default:CURRENT_TIMESTAMP;NOT NULL" json:"update_date"` // 更新时间
-	Isdeleted     sql.NullInt32  `gorm:"column:isdeleted;default:0" json:"isdeleted"`
+	Id            int64     `gorm:"column:id;primary_key;AUTO_INCREMENT" json:"id"`
+	UserId        int64     `gorm:"column:user_id" json:"user_id"`
+	PlayUrl       string    `gorm:"column:play_url" json:"play_url"`
+	CoverUrl      string    `gorm:"column:cover_url" json:"cover_url"`
+	FavoriteCount int       `gorm:"column:favorite_count;default:0" json:"favorite_count"`
+	CommentCount  int       `gorm:"column:comment_count;default:0" json:"comment_count"`
+	Title         string    `gorm:"column:title" json:"title"`
+	CreateDate    time.Time `gorm:"column:create_date;default:CURRENT_TIMESTAMP;NOT NULL" json:"create_date"` // 创建时间
+	UpdateDate    time.Time `gorm:"column:update_date;default:CURRENT_TIMESTAMP;NOT NULL" json:"update_date"` // 更新时间
+	Isdeleted     int32     `gorm:"column:isdeleted;default:0" json:"isdeleted"`
 }
 
 func (m *DyVideo) TableName() string {
