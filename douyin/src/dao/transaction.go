3bce0364a5a9ee1ecda8dec1c857af937de52ad9
root <2213093478@qq.com>
2022-06-07 11:49:46 +0800

    fix 修改取消点赞dao操作失败，sql错误

--- douyin/src/dao/transaction.go
+++ douyin/src/dao/transaction.go
@@ -26,7 +26,7 @@ func Like(userId, videoId int64) error {
 	if rowsAffected := tx.Save(dyFavorite).RowsAffected; tx.Error != nil || rowsAffected != 1 {
 		return errcode.LikeFail
 	}
-	tx.Table("dy_video").Where("id = ? and isdeleted = ?", videoId, 0).UpdateColumn("favorite_count", gorm.Expr("favorite_count + ?", 1))
+	tx.Table("dy_video").Where("id = ?", videoId).UpdateColumn("favorite_count", gorm.Expr("favorite_count + ?", 1))
 	if tx.Error != nil {
 		return errcode.LikeFail
 	}
@@ -43,15 +43,15 @@ func UnLike(userId, videoId int64) error {
 	var db = global.DBEngine
 	tx := db.Begin()
 	defer tx.Callback()
-	dyFavorite := &entity.DyFavorite{
-		UserId:  sql.NullInt64{Int64: userId, Valid: true},
-		VideoId: sql.NullInt64{Int64: videoId, Valid: true},
-	}
+	//dyFavorite := &entity.DyFavorite{
+	//	UserId:  sql.NullInt64{Int64: userId, Valid: true},
+	//	VideoId: sql.NullInt64{Int64: videoId, Valid: true},
+	//}
 	// 保存中间表
-	if rowsAffected := tx.Delete(dyFavorite).RowsAffected; tx.Error != nil || rowsAffected != 1 {
+	if rowsAffected := tx.Where("user_id = ? and video_id = ?", userId, videoId).Delete(&entity.DyFavorite{}).RowsAffected; tx.Error != nil || rowsAffected != 1 {
 		return errcode.UnLikeFail
 	}
-	tx.Table("dy_video").Where("id = ? and isdeleted = ?", videoId, 0).UpdateColumn("favorite_count", gorm.Expr("favorite_count - ?", 1))
+	tx.Table("dy_video").Where("id = ?", videoId).UpdateColumn("favorite_count", gorm.Expr("favorite_count - ?", 1))
 	if tx.Error != nil {
 		return errcode.UnLikeFail
 	}
