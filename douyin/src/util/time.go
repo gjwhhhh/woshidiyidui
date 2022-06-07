a1cc578ee81354d1c2a98fe39fbb8bec3b599ef0
root <2213093478@qq.com>
2022-06-07 16:18:11 +0800

    fix 完善时间格式化工类，新增时间类转换

--- douyin/src/util/time.go
+++ douyin/src/util/time.go
@@ -3,11 +3,20 @@ package util
 import "time"
 
 const DbTimeFormat = "2006-01-02 15:04:05 -0700 MST"
-const VoTimeFormat = "1-2"
+const VoTimeFormat = "2006年1月2日 15时4分"
 
 // ParseDbTimeToVoTime 将db时间格式转换为vo时间格式
 func ParseDbTimeToVoTime(timeStr string) (string, error) {
 	parse, err := time.Parse(DbTimeFormat, timeStr)
+	if err != nil {
+		return "", err
+	}
+	return parse.Format(VoTimeFormat), nil
+}
+
+// ParseRFC3339TimeToVoTime 将RFC3339时间格式转换为vo时间格式
+func ParseRFC3339TimeToVoTime(timeStr string) (string, error) {
+	parse, err := time.Parse(time.RFC3339, timeStr)
 	if err != nil {
 		return "", err
 	}
