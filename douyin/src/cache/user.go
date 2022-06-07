cef32d9daccff84938c8ba95cee7c24b52ff1afd
root <2213093478@qq.com>
2022-06-07 21:21:27 +0800

    add 新增用户缓存工具，进行了简单的测试

---
+++ douyin/src/cache/user.go
@@ -0,0 +1,99 @@
+package cache
+
+import (
+	"douyin/src/pojo/entity"
+	"sync"
+)
+
+var lock sync.Mutex
+
+// UserLruCache 用户缓存
+type UserLruCache struct {
+	size       int
+	capacity   int
+	cache      map[int64]*node
+	head, tail *node
+}
+
+// node 内部节点
+type node struct {
+	k         int64
+	v         *entity.DyUser
+	pre, next *node
+}
+
+func initNode(key int64, value *entity.DyUser) *node {
+	return &node{
+		k: key,
+		v: value,
+	}
+}
+
+func Constructor(capacity int) *UserLruCache {
+	lruCache := &UserLruCache{
+		size:     0,
+		capacity: capacity,
+		cache:    map[int64]*node{},
+		head:     initNode(0, nil),
+		tail:     initNode(0, nil),
+	}
+	lruCache.head.next = lruCache.tail
+	lruCache.tail.pre = lruCache.head
+	return lruCache
+}
+
+func (this *UserLruCache) Head() *entity.DyUser {
+	return this.head.next.v
+}
+
+func (this *UserLruCache) Get(key int64) *entity.DyUser {
+	if _, ok := this.cache[key]; !ok {
+		return nil
+	}
+	node := this.cache[key]
+	this.moveToHead(node)
+	return node.v
+}
+
+func (this *UserLruCache) Put(key int64, value *entity.DyUser) {
+	lock.Lock()
+	defer lock.Unlock()
+	if _, ok := this.cache[key]; !ok {
+		node := initNode(key, value)
+		this.cache[key] = node
+		this.addToHead(node)
+		this.size++
+		if this.size > this.capacity {
+			tail := this.removeTail()
+			delete(this.cache, tail.k)
+			this.size--
+		}
+	} else {
+		node := this.cache[key]
+		node.v = value
+		this.moveToHead(node)
+	}
+}
+
+func (this *UserLruCache) addToHead(node *node) {
+	node.pre = this.head
+	node.next = this.head.next
+	this.head.next.pre = node
+	this.head.next = node
+}
+
+func (this *UserLruCache) removeNode(node *node) {
+	node.pre.next = node.next
+	node.next.pre = node.pre
+}
+
+func (this *UserLruCache) moveToHead(node *node) {
+	this.removeNode(node)
+	this.addToHead(node)
+}
+
+func (this *UserLruCache) removeTail() *node {
+	node := this.tail.pre
+	this.removeNode(node)
+	return node
+}
---
+++ douyin/src/cache/user_test.go
@@ -0,0 +1,32 @@
+package cache
+
+import (
+	"douyin/src/pojo/entity"
+	"fmt"
+	"testing"
+)
+
+func TestUserLruCache1(t *testing.T) {
+	userLruCache := Constructor(50)
+	fmt.Println(userLruCache.size)
+	userLruCache.Put(1, &entity.DyUser{Username: "张三"})
+	fmt.Println(userLruCache.size)
+	fmt.Println(userLruCache.Head().Username)
+	userLruCache.Put(2, &entity.DyUser{Username: "李四"})
+	fmt.Println(userLruCache.size)
+	fmt.Println(userLruCache.Head().Username)
+	userLruCache.Get(1)
+	fmt.Println(userLruCache.size)
+	fmt.Println(userLruCache.Head().Username)
+}
+
+func TestUserLruCache2(t *testing.T) {
+	userLruCache := Constructor(10)
+	for i := 0; i < 20; i++ {
+		fmt.Println(userLruCache.size)
+		userLruCache.Put(int64(i), &entity.DyUser{Username: fmt.Sprintf("用户%d", i)})
+	}
+	fmt.Println(userLruCache.Head().Username)
+	userLruCache.Get(15)
+	fmt.Println(userLruCache.Head().Username)
+}
