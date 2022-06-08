9d7cec74c39a207182c9da9389f214a9646d2eb8
root <2213093478@qq.com>
2022-06-08 13:07:26 +0800

    fix 修复sql错误

--- douyin/docs/sql/maintain.sql
+++ douyin/docs/sql/maintain.sql
@@ -1,5 +1,6 @@
--- 让点赞表主键重新有序递增
+-- 删除无效点赞 让点赞表主键重新有序递增
 START TRANSACTION;
+DELETE FROM `dy_favorite` WHERE `dy_favorite`.is_deleted = 1;
 ALTER TABLE `dy_favorite` DROP `id`;
 ALTER TABLE `dy_favorite` ADD `id` int NOT NULL FIRST;
 ALTER TABLE `dy_favorite` MODIFY COLUMN `id` int NOT NULL AUTO_INCREMENT,ADD PRIMARY KEY(id);
@@ -7,7 +8,7 @@ COMMIT;
 
 -- 删除无效评论，让评论表主键重新有序递增
 START TRANSACTION;
-DELETE FROM `dy_comment` WHERE `dy_comment`.isdeleted = 1;
+DELETE FROM `dy_comment` WHERE `dy_comment`.is_deleted = 1;
 ALTER TABLE `dy_comment` DROP `id`;
 ALTER TABLE `dy_comment` ADD `id` int NOT NULL FIRST;
 ALTER TABLE `dy_comment` MODIFY COLUMN `id` int NOT NULL AUTO_INCREMENT,ADD PRIMARY KEY(id);
