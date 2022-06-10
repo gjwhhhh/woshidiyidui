0e3c5d2e4d535111c89958f4eee954fd029232bc
weixizi <weixizi9@gmail.com>
2022-06-10 10:28:12 +0800

    init dev

---
+++ douyin/src/util/form.go
@@ -0,0 +1,59 @@
+package
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	ut "github.com/go-playground/universal-translator"
+	"strings"
+)
+
+type ValidError struct {
+	Key     string
+	Message string
+}
+
+type ValidErrors []*ValidError
+
+func (v *ValidError) Error() string {
+	return v.Message
+}
+
+func (v ValidErrors) Error() string {
+	return strings.Join(v.Errors(), ",")
+}
+
+func (v ValidErrors) Errors() []string {
+	var errs []string
+	for _, err := range v {
+		errs = append(errs, err.Error())
+	}
+
+	return errs
+}
+func BindAndValid(c *gin.Context, v interface{}) (bool, ValidErrors) {
+	var errs ValidErrors
+	err := c.ShouldBind(v)
+	if err != nil {
+		err = c.ShouldBindBodyWith(v, binding.JSON)
+	}
+	if err != nil {
+		v := c.Value("trans")
+		trans, _ := v.(ut.Translator)
+		verrs, ok := err.(val.ValidationErrors)
+		if !ok {
+			return false, errs
+		}
+
+		for key, value := range verrs.Translate(trans) {
+			errs = append(errs, &ValidError{
+				Key:     key,
+				Message: value,
+			})
+		}
+
+		return false, errs
+	}
+
+	return true, nil
+}
+
